5df3a63df732b9dc7ef916c07afbe0eb782e936b
Daria Anton <antonchenkodaria20@gmail.com>
2021-03-31 21:45:45 +0300

    убираем необъявленные переменные и название с $

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 let user;
 let href = document.location.herif;
-const $userAva = document.getElementById('avatar');
-const $userName = document.getElementById('name');
-const $userBio = document.getElementById('bio');
-const $userLink = document.gerElementById('link');
+const userAva = document.getElementById('avatar');
+const userName = document.getElementById('name');
+const userBio = document.getElementById('bio');
+const userLink = document.gerElementById('link');
 
 let apilink = createUrl ();
 
 const createUrl = function () {
-    if (user) {
-        username = user;
-    } else {
-        username = defaultUser
-    };
+    username = user;
+    
     return 'http://api.github.com/users/' + username;
 }
+
 let apilink = createUrl();
 
 const presentCard = function (obj) {
-  $userAva.src = obj.avatar_url;
-  $userLink.innerHTML = obj.name;
-  $userBio.href = obj.html_url;
+  userAva.src = obj.avatar_url;
+  userLink.innerHTML = obj.name;
+  userBio.href = obj.html_url;
   if (obj.bio != null) {
-      $userBio.innerHTML = obj.bio;
+      userBio.innerHTML = obj.bio;
   } else {
-      $userBio.innerHTML = "this user haven't bio in profile";
+      userBio.innerHTML = "this user haven't bio in profile";
   }
 }
 
@@ -32,4 +30,4 @@ fetch(apilink)
 .then(res =>  res.json())
 .then(obj => user = Object.assign({}, obj))
 .then(user => createCard(user))
-.catch(err => console.error(err))
+.catch(err => console.error(err));
