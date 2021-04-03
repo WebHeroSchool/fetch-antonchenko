e4b0e5e068ec469f9b731701730da2c0796b0d51
Daria Anton <antonchenkodaria20@gmail.com>
2021-04-03 21:30:22 +0300

    end

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
-let user = Link;
+
 const userAva = document.getElementById('avatar');
 const userName = document.getElementById('name');
 const userBio = document.getElementById('bio');
 const userLink = document.gerElementById('link');
 
-let apilink = createUrl ();
-
-const createUrl = function () {
-    username = user;
-    
-    return 'http://api.github.com/users/' + username;
-}
-
 const presentCard = function (obj) {
   userAva.src = obj.avatar_url;
   userLink.innerHTML = obj.name;
   userBio.href = obj.html_url;
-  if (obj.bio != null) {
-      userBio.innerHTML = obj.bio;
-  } else {
-      userBio.innerHTML = "this user haven't bio in profile";
-  }
+  if (!obj.bio) userBio.innerHTML = obj.bio;
+  else userBio.innerHTML = "this user haven't bio in profile";
 }
 
-fetch(apilink)
+fetch("http;//api.github.com/users/persvot")
 .then(res =>  res.json())
 .then(user => presentCard(user))
 .catch(err => console.error(err));
