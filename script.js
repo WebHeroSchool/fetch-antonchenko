3d4bea395c2a9ab40fdecb880107c5be68b036ad
Daria Anton <antonchenkodaria20@gmail.com>
2021-04-04 21:46:43 +0300

    promise

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 
+const preloader = document.getElementById('loader')
+const dateHtml = document.querySelector('.date');
+
 const userAva = document.getElementById('avatar');
 const userName = document.getElementById('name');
 const userBio = document.getElementById('bio');
 const userLink = document.gerElementById('link');
 
-const presentCard = function (obj) {
+const getPreloader = new Promise ((resolve, reject) => {
+setTimeout(() => {
+  resolve (new Date ())
+}, 1000)
+});
+
+const getTime = new Promise ((resolve, reject) => {
+  fetch ("http://api.github.com/users/")
+  .then((res) => res.json())
+  .then((time) => {
+    resolve(time)
+  })
+});
+
+ function presentCard(obj, time) {
+  dateHtml.innerHTML = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds()
   userAva.src = obj.avatar_url;
   userLink.innerHTML = obj.name;
   userBio.href = obj.html_url;
@@ -12,7 +30,11 @@ const presentCard = function (obj) {
   else userBio.innerHTML = "this user haven't bio in profile";
 }
 
-fetch("http://api.github.com/users/Persvot")
-.then(res =>  res.json())
-.then(user => presentCard(user))
+  Promise.all([getPreloader, getTime])
+  .then(([date, time]) => {
+   presentCard(date, time)
+   preloader.style.display = 'none'
+  })
+
 .catch(err => console.error(err));
+
